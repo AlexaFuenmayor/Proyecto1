5ba098b35ac087f4ed6f5bea97bce5901b799966
alexfuenmayor <estherf.m18@gmail.com>
2021-09-20 21:50:18 -0500

    primer commit

---
+++ estilos.js
@@ -0,0 +1,59 @@
+let notaFederer = () => {
+  alert(
+    "Roger Federer (Basilea; 8 de agosto de 1981) es un tenista profesional suizo."
+  );
+};
+
+let notaDinho = () => {
+  alert(
+    "Ronaldo de Assis Moreira (Porto Alegre, 21 de marzo de 1980), mejor conocido como Ronaldinho Gaúcho. Ex futbolista Brasileño"
+  );
+};
+
+let notaDeyna = () => {
+  alert(
+    "Deyna Castellanos (Maracay, Aragua, 18 de abril de 1999) es una futbolista venezolana que juega como delantera en el Atlético de Madrid de la Primera División de España y es internacional con la Selección de Venezuela."
+  );
+};
+
+let notaPacquiao = () => {
+  alert(
+    "Emmanuel Dapidran Pacquiao (Kibawe, 17 de diciembre de 1978), más conocido como Manny «Pac-Man» Pacquiao, es un boxeador, actor, cantante y político filipino."
+  );
+};
+
+let notaMessi = () => {
+  alert(
+    "Lionel Andrés Messi Cuccittini (Rosario, Santa Fe; 24 de junio de 1987), conocido como Leo Messi, es un futbolista argentino"
+  );
+};
+
+let notaPhelps = () => {
+  alert(
+    "Michael Fred Phelps .​ (Baltimore, Maryland, 30 de junio de 1985 ) es un nadador olímpico estadounidense."
+  );
+};
+
+let notaAlex = () => {
+  alert(
+    "Alexandra Patricia Morgan Carrasco (Diamond Bar, California, Estados Unidos; 2 de julio de 1989) conocida como Alex Morgan, es una futbolista profesional y escritora estadounidense."
+  );
+};
+
+let notaBolt = () => {
+  alert(
+    "Usain Bolt (Sherwood Content, parroquia de Trelawny, Jamaica, 21 de agosto de 1986) es un ex atleta profesional jamaiquino"
+  );
+};
+
+let notaBryant = () => {
+  alert(
+    "Kobe Bean Bryant (Filadelfia, Pensilvania; 23 de agosto de 1978- California; 26 de enero de 2020) fue un baloncestista estadounidense"
+  );
+};
+
+let notaYulimar = () => {
+  alert(
+    "Yulimar del Valle Rojas Rodríguez (Caracas, 21 de octubre de 1995) es una atleta venezolana especializada en triple salto"
+  );
+};
